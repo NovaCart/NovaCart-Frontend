fe335b8ebff91b3dffed6bfe47de19a12085097b
Nimesha Abesinghe <abesinghenimesha20@gmail.com>
2024-08-29 15:29:34 +0530

    added list

---
+++ src/components/HomeNavBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function HomeNavBar() {
+  return (
+    <div>
+      <div>NovaCart</div>
+      <div>
+        <p>Home</p>
+        <p>Gallery</p>
+        <p>Men</p>
+        <p>Women</p>
+        <p>SignUp</p>
+        <img src="/src/images/user-circle-single--circle-geometric-human-person-single-user (1).png" alt="" />   
+    </div>
+    </div>
+  )
+}
+
+export default HomeNavBar
