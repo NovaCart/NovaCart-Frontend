59b83361514d710b3c1c3a003b27fc2dea4e887e
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2024-08-30 11:00:58 +0530

    style: wishlist
    update styles in wishlist,rename profile to wishlist and link history page and settings page

---
+++ src/pages/History.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HomeNavBar from '../components/HomeNavBar'
+
+export default function WishList() {
+  return (
+    <div>
+        <HomeNavBar/>
+        <h1>bye</h1>
+    </div>
+  )
+}
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HomeNavBar from '../components/HomeNavBar'
+
+export default function Settings() {
+  return (
+    <div>
+        <HomeNavBar/>
+        <h1>hi</h1>
+    </div>
+  )
+}
