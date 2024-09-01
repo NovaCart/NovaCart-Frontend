28aa5410883582578fe3b7958e690a2e80a022d0
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2024-09-01 23:34:58 +0530

    feat: update
    add advanced settings to settings page and change styles

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeNavBar from "../components/HomeNavBar";
 import Footer from "../components/Footer";
 import ContactCard from "../components/ContactCard";
@@ -6,6 +6,12 @@ import FormInput from "../components/SettingsPage/FormInput";
 import Button from "../components/Button";
 
 export default function Settings() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
     <div>
       <HomeNavBar />
@@ -13,33 +19,92 @@ export default function Settings() {
         <div className="w-3/4">
           <h1 className="text-left font-serif text-4xl my-8 ml-36">Settings</h1>
           <div className="border-b-2 border-black mt-12 ms-12 me-12"></div>
-          <div className="p-2">
-            <div className="w-2/3 ml-36 mt-8">
-              <FormInput
-                label="First Name"
-                value="Hasitha"
-                onChange={(e) => console.log(e.target.value)}
-              />
-              <FormInput
-                label="Last Name"
-                value="Prasadini"
-                onChange={(e) => console.log(e.target.value)}
-              />
-              <FormInput
-                label="Email Address"
-                value="hasitha@gmail.com"
-                onChange={(e) => console.log(e.target.value)}
-              />
-              <FormInput
-                label="Password"
-                value="Hasitha"
-                onChange={(e) => console.log(e.target.value)}
-              />
-              <div className="ml-[650px]">
-                <Button text="Edit" />
+          <form>
+            <div className="p-2">
+              <div className="w-2/3 ml-36 mt-8">
+                <FormInput
+                  type="text"
+                  label="First Name"
+                  value="Hasitha"
+                  readonly={true}
+                  onChange={(e) => console.log(e.target.value)}
+                />
+                <FormInput
+                  type="text"
+                  label="Last Name"
+                  value="Prasadini"
+                  readonly={true}
+                  onChange={(e) => console.log(e.target.value)}
+                />
+                <FormInput
+                  type="text"
+                  label="Email Address"
+                  value="hasitha@gmail.com"
+                  readonly={true}
+                  onChange={(e) => console.log(e.target.value)}
+                />
+                <FormInput
+                  type="text"
+                  label="Password"
+                  value="Hasitha"
+                  readonly={true}
+                  onChange={(e) => console.log(e.target.value)}
+                />
+              </div>
+              <div className=" ml-36 w-2/3">
+                <div className="flex items-center">
+                  <input
+                    id="shipping-checkbox"
+                    type="checkbox"
+                    className="mr-2"
+                    checked={isChecked}
+                    onChange={handleCheckboxChange}
+                  />
+                  <label htmlFor="shipping-checkbox" className="text-lg">
+                    Update Advanced Details
+                  </label>
+                </div>
+
+                {isChecked && (
+                  <div className="mt-8">
+                      <FormInput
+                        type="text"
+                        label="Country"
+                        value="Sri Lanka"
+                        onChange={(e) => console.log(e.target.value)}
+                        />
+                        <FormInput
+                        type="number"
+                        label="Contact No"
+                        value="0760828945"
+                        onChange={(e) => console.log(e.target.value)}
+                        />
+                        <FormInput
+                        type="text"
+                        label="Address"
+                        value="Wasana, Akuressa"
+                        onChange={(e) => console.log(e.target.value)}
+                        />
+                        <FormInput
+                        type="text"
+                        label="City"
+                        value="Matara"
+                        onChange={(e) => console.log(e.target.value)}
+                        />
+                        <FormInput
+                        type="number"
+                        label="Postal Code"
+                        value="81056"
+                        onChange={(e) => console.log(e.target.value)}
+                        />
+                  </div>
+                )}
+                <div className="ml-[650px]">
+                  <Button text="Edit" />
+                </div>
               </div>
             </div>
-          </div>
+          </form>
         </div>
         <div className="w-1/4">
           <ContactCard
